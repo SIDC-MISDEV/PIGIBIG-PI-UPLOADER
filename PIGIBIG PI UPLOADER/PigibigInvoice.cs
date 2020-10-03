cdcace0d0309e46683b54bc02a234045e97ad8e8
litsky101 <desagunangelito@gmail.com>
2020-10-03 18:34:29 +0800

    Add project files.

---
+++ PIGIBIG PI UPLOADER/PigibigInvoice.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIGIBIG_PI_UPLOADER
+{
+    public class PigibigInvoice
+    {
+        public string Reference { get; set; }
+        public string SowNo { get; set; }
+        public string ParityNo { get; set; }
+        public string TypeTransaction { get; set; }
+        public string BatchNo { get; set; }
+        public string IdStock { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Qty { get; set; }
+        public string UOM { get; set; }
+        public string Price { get; set; }
+        public string TotalAmount { get; set; }
+        public string InvoiceDate { get; set; }
+        public string BranchCode { get; set; }
+        public string AccountNo { get; set; }
+        public string CodeNo { get; set; }
+
+        public void SaveTransaction(List<PigibigInvoice> data)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                List<string> prm = new List<string>();
+                Dictionary<string, object> dic = new Dictionary<string, object>();
+                
+
+                for (int count = 0; count < data.Count; count++)
+                {
+                    sb.Append(@"INSERT INTO pig_tran02 (TRANS_ID, REFERENCE_NO, SOW, PARITY, TYPE, BATCH, ID_STOCK, DESCRIPTION, CATEGORY, QUANTITY, UOM, PRICE, TOTAL, INV_DATE, CODE_NO, ACCOUNT_NO, BRANCH_CODE) VALUES ");
+                    sb.Append($"(0, @reference{count}, @sow{count}, @parity{count}, @type{count}, @batch{count}, @idstock{count}, @desc{count}, @category{count}, @quantity{count}, @uom{count}, @price{count}, @total{count}, @invdate{count}, @codeno{count}, @account{count}, @branchcode{count}); {Environment.NewLine}");
+
+                    dic.Add("@reference" + count, data[count].Reference);
+                    dic.Add("@sow" + count, data[count].SowNo);
+                    dic.Add("@parity" + count, data[count].ParityNo);
+                    dic.Add("@type" + count, data[count].TypeTransaction);
+                    dic.Add("@batch" + count, data[count].BatchNo);
+                    dic.Add("@idstock" + count, data[count].IdStock);
+                    dic.Add("@desc" + count, data[count].Reference);
+                    dic.Add("@category" + count, data[count].Category);
+                    dic.Add("@quantity" + count, data[count].Qty);
+                    dic.Add("@uom" + count, data[count].UOM);
+                    dic.Add("@price" + count, data[count].Price);
+                    dic.Add("@total" + count, data[count].TotalAmount);
+                    dic.Add("@invdate" + count, data[count].InvoiceDate);
+                    dic.Add("@codeno" + count, data[count].CodeNo);
+                    dic.Add("@account" + count, data[count].AccountNo);
+                    dic.Add("@branchcode" + count, data[count].BranchCode);
+                }
+
+                using (var mysql = new MySQLHelper(Properties.Settings.Default.DB))
+                {
+                    mysql.BeginTransaction();
+
+                    mysql.ArgSQLCommand = sb;
+                    mysql.ArgSQLParam = dic;
+                    mysql.ExecuteMySQL();
+
+                    //update TRANS_ID in pigibig invoice 
+                    mysql.ArgSQLCommand = new StringBuilder(@"UPDATE pig_tran02 pi
+                            INNER JOIN pig_cust00 cust ON pi.CODE_NO = cust.CODE_NO
+                            SET pi.TRANS_ID = cust.ID
+                            WHERE pi.TRANS_ID = 0;");
+                    mysql.ExecuteMySQL();
+
+
+                    mysql.CommitTransaction();
+
+                }
+
+            }
+            catch
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ PIGIBIG PI UPLOADER/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PIGIBIG_PI_UPLOADER
+{
+    class Program
+    {
+        private static string dropSite = Properties.Settings.Default.DROP_SITE;
+        private static string fileExtension = Properties.Settings.Default.FILE_EXT;
+        private static string backup = Path.Combine(dropSite, "backup");
+        private static List<string> invFiles = null;
+
+        static void Main(string[] args)
+        {
+            invFiles = new List<string>();
+
+            invFiles = Directory.GetFiles(dropSite, fileExtension).ToList();
+
+            foreach (var item in invFiles)
+            {
+                try
+                {
+                    ProcessFile(item);
+                    Thread.Sleep(1000);
+
+                    //Move processed fie to backup for reference
+                    if (Directory.Exists(backup))
+                        File.Move(item, Path.Combine(backup, Path.GetFileName(item)));
+                    else
+                    {
+                        Directory.CreateDirectory(backup);
+                        File.Move(item, Path.Combine(backup, Path.GetFileName(item)));
+                    }
+                }
+                catch (Exception er)
+                {
+                    var sb = new StringBuilder();
+                    sb.Append($"{DateTime.Now.ToString()} -> Failed to upload {Path.GetFileName(item)}  {Environment.NewLine} Reason: {er.Message} {Environment.NewLine}");
+
+                    File.AppendAllText(dropSite, sb.ToString());
+                }
+            }
+
+        }
+
+        static void ProcessFile(string files)
+        {
+            try
+            {
+                List<PigibigInvoice> inv = new List<PigibigInvoice>();
+                string[] data = new string[] { };
+
+
+                foreach (var item in File.ReadAllLines(files))
+                {
+                    var test = item.Replace("\"", "");
+
+                    data = test.Split(',');
+
+                    inv.Add(new PigibigInvoice
+                    {
+                        Reference = data[0].ToString(),
+                        SowNo = data[1].ToString(),
+                        ParityNo = data[2].ToString(),
+                        TypeTransaction = data[3].ToString(),
+                        BatchNo = data[4].ToString(),
+                        IdStock = data[5].ToString(),
+                        Description = data[6].ToString(),
+                        Category = data[7].ToString(),
+                        Qty = data[8].ToString(),
+                        UOM = data[9].ToString(),
+                        Price = data[10].ToString(),
+                        TotalAmount = data[11].ToString(),
+                        InvoiceDate = data[12].ToString(),
+                        CodeNo = data[13].ToString(),
+                        AccountNo = data[14].ToString(),
+                        BranchCode = data[15].ToString()
+                    });
+                }
+
+                if (inv.Count > 0)
+                {
+                    PigibigInvoice pi = new PigibigInvoice();
+                    pi.SaveTransaction(inv);
+                }
+
+            }
+            catch
+            {
+
+                throw;
+            }
+        }
+        
+    }
+}
